50f4ed4d26d492172ab43c45b7ecda0c714a0570
yumechi <yumechi0525@gmail.com>
2022-04-14 01:15:07 +0900

    feat: import, export形式でjestが実行できるように変更
    babelの使用とそれに伴うパッケージの追加
    
    connections #9 fixes #9

---
+++ src/calc.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * @param {number} a 引数1
+ * @param {number} b 引数2
+ * @returns {number}
+ */
+export function sum(a, b) {
+    return a + b;
+}
+
+/**
+ * 
+ * @param {number} a 引数1
+ * @param {number} b 引数2
+ * @returns {number}
+ */
+ export function sub(a, b) {
+    return a - b;
+}
+
+/**
+ * 
+ * @param {number} a 引数1
+ * @param {number} b 引数2
+ * @returns {boolean}
+ */
+export function gt(a, b) {
+    if(a > b) {
+        return true;
+    } else {
+        return false;
+    }
+}
